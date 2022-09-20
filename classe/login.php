23b647b93659df61de223a70a311c60ad45b1564
unknown <sanjoy.wong@gmail.com>
2022-09-20 22:45:08 +0800

    Ajoute Config class pour connecter avec la base de donnee

---
+++ classe/login.php
@@ -0,0 +1,20 @@
+<?php
+class Login{
+
+    var $username;
+    var $psw;
+
+    private Config $db;
+
+    public function __construct()
+    {
+        $this->db = new Config();
+    }
+    
+    
+
+
+
+}
+
+?>
